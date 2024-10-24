2275a003c29ad27424f532639d9c83c9e23b71ba
Lalit Rao <lalitkumaryadav9090@gmail.com>
2024-10-24 14:48:05 +0530

    First Commit

---
+++ NikeApp/Views/Onboarding.swift
@@ -0,0 +1,18 @@
+//
+//  Onboarding.swift
+//  NikeApp
+//
+//  Created by Lalit Kumar on 24/10/24.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Onboarding()
+}
---
+++ NikeApp/Views/Splash.swift
@@ -0,0 +1,18 @@
+//
+//  Splash.swift
+//  NikeApp
+//
+//  Created by Lalit Kumar on 24/10/24.
+//
+
+import SwiftUI
+
+struct Splash: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Splash()
+}
