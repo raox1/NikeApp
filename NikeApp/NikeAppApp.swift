c5e3a951729fcdd6e102eaf5f235b9a818b76faa
Lalit Rao <lalitkumaryadav9090@gmail.com>
2024-10-25 11:57:16 +0530

    Added shoe image in the onboarding screen

--- NikeApp/NikeAppApp.swift
+++ NikeApp/NikeAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct NikeAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Splash()
         }
     }
 }
--- NikeApp/Views/Onboarding.swift
+++ NikeApp/Views/Onboarding.swift
@@ -8,18 +8,60 @@
 import SwiftUI
 
 struct Onboarding: View {
+    @State private var isActive: Bool = false
+
     var body: some View {
-        ZStack {
-            
+        
+        if isActive {
+            // We move to Login screen after that
+        } else {
+            // Design of This view
+            ZStack(alignment: .top) {
+                VStack {
+                    Spacer()
+                    // Add a red circle in bottom
+                    Circle()
+                        .fill(
+                            RadialGradient(colors: [.red, .clear, .clear, .clear], center: .center, startRadius: 0, endRadius: UIScreen.main.bounds.width)
+                        )
+                        
+                }
+                .frame(height: .infinity)
+                
+                VStack(spacing: 15, content: {
+                    Spacer()
+
+                    Image("shoeob")
+                        .resizable()
+                        .scaledToFit()
+                        
+                    Text("Start Journey\nwith NIKE")
+                        .font(.largeTitle.bold())
+                        .multilineTextAlignment(.center)
+                    
+                    Text("Smart gorgeous and fashionable collection make you cool")
+                        .multilineTextAlignment(.center)
+                        .opacity(0.6)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Image("")
+                    }
+                })
+                
+
+            }
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(
+                Image("onboarding_back")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+            )
+            .preferredColorScheme(.dark)
         }
-        .padding()
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(
-            Image("onboarding_back")
-                .resizable()
-                .scaledToFill()
-                .ignoresSafeArea()
-        )
     }
 }
 
